9b3fa82eea1c0353f102212a6fa0bd3880d06873
damilare <dblacktin@gmail.com>
2020-03-04 12:17:24 +0100

    convert a slice to string, join strings

--- deck.go
+++ deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Create a new type of 'deck'
 //which is a slice of strings
@@ -25,6 +28,10 @@ func newDeck() deck {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 }
 
 func (d deck) print() {
